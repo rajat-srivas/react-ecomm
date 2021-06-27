2e1ab8bc745fc24f74e4159aa6b62ea15d63da4e
Rajat Srivastava <rajat.sedate@gmail.com>
2021-06-27 20:55:22 +0530

    Added Cart in Header

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -4,6 +4,7 @@ import './header.style.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.util';
 import { connect } from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 const Header = ({ currentUser }) => (
 
@@ -20,6 +21,7 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/signin' >SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 
